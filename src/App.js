5c8a5ca1993eaf7d563bb3767e39ddc351eb133d
Mehmet Topakkaya <mehmet.topakkaya@segmentify.com>
2022-09-15 23:17:58 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-
+import { DATA } from "./Data.js";
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
 function App() {
+
+  const [url, setUrl] = useState(null)
+  const changeUrl = (url) => {
+    setUrl(url)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='header-main'>
+        <div className='header'>Kanallar</div>
+      </div>
+      <div className='stations'>
+        {DATA.map((item, index) => {
+          return (
+            <div key={index} className='station-card'>
+              <div className='statiton-img'>
+                <img src={item.img} alt={item.stationName} />
+              </div>
+              <span className='station-name'>{item.stationName}</span>
+              <span className='station-type'>{item.stationType}</span>
+              <div className='play-button'>
+                <a onClick={() => setUrl(item.url)} >Dinle</a>
+              </div>
+            </div>
+          )
+        })
+
+        }
+      </div>
+      <div className='player'>
+        <AudioPlayer
+          autoPlay
+          src={url}
+          onPlay={e => console.log("onPlay")}
+        // other props here
+        />
+      </div>
     </div>
   );
 }
---
+++ src/Data.js
@@ -0,0 +1,10 @@
+export const DATA=[
+     {id:1,url:"http://37.247.98.8/stream/166/",stationName:"90'lar",stationType:"Türkçe Pop",img:"https://radyohomestatic.imgix.net/media/station/31/logo_square.png?v=060520050957&auto=format&bg=ff9833"},
+     {id:2,url:"http://37.247.98.7:80/;stream.mp3",stationName:"Baba Radyo",stationType:"Arabesk",img:"https://radyodi.com/wp-content/uploads/2019/05/baba-radyo.png"},
+     {id:3,url:"http://46.20.7.125/listen.pls",stationName:"Best Fm",stationType:"Türkçe Pop",img:"https://pbs.twimg.com/profile_images/492763416084246529/TM63Sx1m_400x400.png"},
+     {id:4,url:"http://45.32.154.169:9300/;",stationName:"İstanbul Fm",stationType:"Türkçe Pop",img:"https://www.stalternatif.com/dosyalar/istanbulfmplayerlogo.png"},
+     {id:5,url:"http://46.20.3.204:80/",stationName:"Kral Fm",stationType:"Arabesk",img:"https://pbs.twimg.com/profile_images/1264920892049670146/Ltw0vqHI_400x400.jpg"},
+     {id:6,url:"http://37.1.145.21:9001/;stream.mp3",stationName:"Radyo FG FM",stationType:"Yabancı Müzik",img:"https://i0.wp.com/freetime.mikeconnelly.com/wp-content/uploads/2017/05/radio_fg.jpg?ssl=1"},
+     {id:7,url:"http://stream.radyoodtu.com.tr/rock;",stationName:"Radyo Odtu",stationType:"Yabancı Rock",img:"https://is4-ssl.mzstatic.com/image/thumb/Purple4/v4/87/e9/2b/87e92b8c-3549-575e-548e-00de9f357001/source/512x512bb.jpg"},
+     {id:8,url:"http://46.20.3.201:80/",stationName:"Radyo Trafik",stationType:"Yol Marmara",img:"https://radyotrafik.com/images/marmara-logo.png"},    
+]
